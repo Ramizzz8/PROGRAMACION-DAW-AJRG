739fdb332879aa54d3bb4e175005ffcfeaba6d0b
Ramizzz8 <ajulianrague@gmail.com>
2025-10-15 09:52:24 +0200

    1

---
+++ ETC/miflask.py
@@ -0,0 +1,30 @@
+from flask import Flask
+
+aplicacion = Flask(__name__)
+
+@aplicacion.route("/")
+
+def raiz():
+    cadena =  '''
+    <!doctype html>
+    <html>
+        <head>
+            <title></title>
+            <style>
+                h1{color:red;}
+            </style>
+        </head>
+        <body>
+            <h1>Esto es HTML a tope de power con FLASK </h1>
+    '''
+    for dia in range(1,31):
+        cadena += '<div>'+str(dia)+'</dia>'
+    cadena += '''
+        </body>
+    </html>
+
+    '''
+    return cadena
+
+if __name__ == "__main__":
+    aplicacion.run(debug=True)
