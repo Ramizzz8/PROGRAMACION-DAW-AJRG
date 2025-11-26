060cfeb7140064f6f91e7ab690fb12e152ecc072
Ramizzz8 <ajulianrague@gmail.com>
2025-11-26 09:48:14 +0100

    1

---
+++ 006-Aplicación de las estructuras de almacenamiento/006-Colecciones Listas, Conjuntos y Diccionarios/101-Ejercicios/004-lista de diccionarios en diccionario.py
@@ -0,0 +1,18 @@
+persona = {
+  "nombre": "Andres Julian",
+  "apellidos": "Ramirez Guerrero",
+  "correo": "infoandres@gmail.com",
+  "edad":17,
+  "telefono": [
+    {
+      "tipo":"fijo",
+      "numero": 12345678
+    },
+    {
+      "tipo":"movil",
+      "numero": 87654321
+    }
+  ]
+}
+
+print(persona)
---
+++ 006-Aplicación de las estructuras de almacenamiento/006-Colecciones Listas, Conjuntos y Diccionarios/101-Ejercicios/007-doble edad con argumentos.py
@@ -0,0 +1,9 @@
+import sys
+
+argumentos = sys.argv
+
+edad = argumentos[1]
+
+edad_entero = int(edad)
+doble_edad = edad_entero * 2
+print(doble_edad)
