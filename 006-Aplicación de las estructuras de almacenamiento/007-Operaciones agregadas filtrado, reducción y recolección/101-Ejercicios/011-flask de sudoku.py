29152027c7418af448d936530e67f244a203ea54
Ramizzz8 <ajulianrague@gmail.com>
2025-11-27 14:05:18 +0100

    1

--- 006-Aplicación de las estructuras de almacenamiento/007-Operaciones agregadas filtrado, reducción y recolección/101-Ejercicios/011-flask de sudoku.py
+++ 006-Aplicación de las estructuras de almacenamiento/007-Operaciones agregadas filtrado, reducción y recolección/101-Ejercicios/011-flask de sudoku.py
@@ -6,20 +6,23 @@ app = Flask(__name__)
 @app.route("/")
 
 def inicio():
+  contador = 0
   patron = {1,2,3,4,5,6,7,8,9}
   sudoku = []
   for celda in range(1,10):#Tambien puede ser de (0,9)/// ademas el "celda" puede cambiarse por "_" ya que "celda" no es usado
     while True:
+      contador += 1
       lista = []
       for i in range(1,10):
         lista.append(random.randint(1,9))
-      conjunto = set(lista)
+        conjunto = set(lista)
       if conjunto == patron:
         sudoku.append(lista)
         break # Fuerzo la finalizazión del bucle infinito
 
-    return render_template("index.html", datos = sudoku)
+      print("He necesitado fuerza bruta"+str(contador)+"intentos.")
+
+  return render_template("index.html", datos = sudoku)
 
 if __name__ == "__main__":
   app.run(debug = True)
-
